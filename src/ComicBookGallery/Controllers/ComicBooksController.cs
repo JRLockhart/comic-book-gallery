a40077e6c99490840dd4ff083f8e4a967812057d
James Lockhart <james.lockhart117@gmail.com>
2018-01-09 13:36:12 +0300

    Updated actioncontroller to use redirect

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,9 +9,14 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller
     {
 
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller";
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello from me");                   
         }
     }
 }
